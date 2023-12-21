25bb3bd4f576a99089abd33c868470cd5e9161b1
busekaramann <busekaraman035@gmail.com>
2023-12-21 20:29:20 +0300

    dal initalized

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataAccess.Concrate.InMemory;
+using Entities.Concreate;
+
+InMemoryCarDal _carDal = new InMemoryCarDal();
+
+Car carToAdded = new Car() { Id = 6, BrandId = 2, ColorId = 1 , ModelYear = 2022, DailyPrice = 579.0, Description = "Yeni Tesla"};
+
+_carDal.Add(carToAdded);
+
+List<Car> cars = _carDal.GetAll();
+
+foreach (var item in cars)
+{
+    Console.WriteLine(item.DailyPrice);
+}
+
+
+Car car = _carDal.GetById(2);
+Console.WriteLine(car.DailyPrice);
+
---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        void Add(Car car);
+        void Update( Car car);
+        void Delete(int Id);   
+        
+        List<Car> GetAll();
+        Car GetById(int Id);
+
+    }
+}
