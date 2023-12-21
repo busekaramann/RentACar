16461b1de6d5d8f720efcde99a54561399b4a171
busekaramann <busekaraman035@gmail.com>
2023-12-21 19:32:32 +0300

    Entities Layer Initalized

---
+++ Entities/Concreate/CarEntity.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concreate
+{
+    public class CarEntity:IEntity
+    {
+        public int Id { get; set; }
+    }
+}
